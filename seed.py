e3d86c58b863db7538e6bed54db5db3bc1878387
Lavinia Karl <laviniaclare@Lavinias-MacBook-Air-2.local>
2016-09-11 14:18:33 -0700

    cleaned up comments. Starting on tests.

--- seed.py
+++ seed.py
@@ -1,56 +1,39 @@
 import psycopg2
 import os
 
-############## REFERENCES TO DELETE #################
-"""CREATE TABLE table_name
-(
-column_name1 data_type(size),
-column_name2 data_type(size),
-column_name3 data_type(size),
-....
-);"""
-
-
-"""CREATE TABLE Persons
-(
-PersonID int,
-LastName varchar(255),
-FirstName varchar(255),
-Address varchar(255),
-City varchar(255)
-);"""
-
-########################################################
 
 def populate_db():
+	""""Populates database using files in spec folder and data folder"""
 	specfiles = os.listdir("specs")
 	
 	for specfile in specfiles:
 		#remove '.csv' from end to get table name
 		table_name = specfile[:-4]
-		#open file with table specs
+		#open file with table specs representing table schema
 		table_specs = open('specs/'+specfile)		
 		# create schema to be used by create table function and load data function
 		table_schema = create_table_schema(table_specs)
-		print table_schema
-		# create the table
+		# create the table in db
 		create_table(table_name, table_schema)
 		# load data into table
 		load_table_data(table_name, table_schema)
-
 
 
 def create_table_schema(specfile):
 	"""takes in a spec file for a table, returns list of tuples representing schema for table"""
 	schema = []
 
+	# looping through lines in specfile, skipping the header line
 	for line in specfile.readlines()[1:]:
+
 		column = line.strip().split(",")
 		column_name = column[0]
 		column_width = column[1]
 		column_type = column[2]
 
 		if column_type == 'TEXT':
+			# postgres does not recognize type "TEXT", so I chose to change it to "VARCHAR", which is meant
+			# to represent stings of varying length and will require us to specify a max length.
 			column_type = 'VARCHAR'
 
 		schema.append((column_name, column_width, column_type))
@@ -59,16 +42,20 @@ def create_table_schema(specfile):
 
 
 def create_table(table_name, schema):
-	"""takes in name, schema, creates table in db"""
+	"""takes in name and schema then creates table in db fitting specifications"""
 
+	print "\n\nCREATING TABLE ", table_name
+
+	# initalizing query string
 	query = """CREATE TABLE %s (""" % table_name
 
+	# looping through columns in schema to construct full CREATE TABLE query
 	for column in schema:
-		# TODO: Add check for all types that take len args perhaps with set containing column type names.
-		# can then check if column type in "column_type_takes_args"
 		if column[2] == 'VARCHAR':
+			# varchar takes in an argument max lenght, so if our column is that type we need to provide it.
 			column_string = '%s %s(%s), ' % (column[0], column[2], column[1])
 		else:
+			# booleans and integers do not take in a parameters.
 			column_string = '%s %s, ' % (column[0], column[2])
 
 		query += column_string
@@ -76,7 +63,8 @@ def create_table(table_name, schema):
 	#remove extra space and comma from last column
 	query = query[:-2]
 	query += ');'
-	print query
+
+	# execute query and commit changes
 	cur = conn.cursor()
 	cur.execute(query)
 	conn.commit()
@@ -86,14 +74,19 @@ def create_table(table_name, schema):
 
 def load_table_data(table_name, table_schema):
 	"""Takes in a table name and schema and inserts data from data files into table"""
+
+	print "\n\n LOADING DATA INTO TABLE ", table_name
 	
-	# get list of file objects with data to be added to given table
+	# get list of file objects with data to be added to given table. 
 	table_data_files = [open("data/"+file_name) for file_name in os.listdir("data") if file_name.startswith(table_name)]
 
-	print table_data_files
+	# NOTE: Does not take into account data added. if this function were run periodically as data files were added, 
+	# taking date into account would be necessary to avoid duplicate data entry. 
 
+	# each data file may contain multiple rows of data so we loop through each file, and each row in that file
 	for data_file in table_data_files:
 		data = data_file.readlines()
+		# insert data one row at a time. Is there a more efficient way to do this?
 		for row in data:
 			insert_row_into_table(row, table_name, table_schema)
 
@@ -115,8 +108,8 @@ def insert_row_into_table(row, table_name, table_schema):
 		column_type = column[2]
 		column_value = row[:column_width].strip()
 
-		# We want to store True or False in the db, but bools are represented as 1 or 0 in data text files
-		# so we much convert them to True or False values before inserting into db
+		# If column type is boolean want to store True or False in the db, but bools are represented as 1 or 0 in data 
+		# text files so we much convert them to True or False values before inserting into db
 		if column_type == "BOOLEAN":
 			# I'm assumeing here that booleans will always be represented by 1 or 0 in data text files.
 			# if this is not the case the conversion from string to int could throw an error.
@@ -132,7 +125,7 @@ def insert_row_into_table(row, table_name, table_schema):
 
 		row = row[column_width:]
 
-	# Remove extra comma and space from end
+	# Remove extra comma and space from end. I feel like there should be a more elegant way to deal with this.
 	insert_into = insert_into[:-2]
 	insert_into += ')'
 	# Remove extra comma and space from end
@@ -164,15 +157,5 @@ if __name__=="__main__":
 	conn = connect_to_db()
 
 	if conn:
-
 		populate_db()
-
-		# specfiles = os.listdir("specs")
-		# load_specs(specfiles)
-		# print "created tables"
-
-		# datafiles = os.listdir("data")
-		# load_data(datafiles)
-		# print "loaded data"
-		
 		conn.close()
